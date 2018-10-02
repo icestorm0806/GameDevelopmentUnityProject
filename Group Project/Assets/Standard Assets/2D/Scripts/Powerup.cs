ad7b90eb05b1fbe2f0e34dcc4ec01ad1a77c9691
Aaron Fulwider <37822131+icestorm0806@users.noreply.github.com>
2018-10-02 14:20:02 -0500

    destroy(col.gameobject)
    trying to fix this stupid thing

---
+++ Group Project/Assets/Standard Assets/2D/Scripts/Powerup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour {
+    
+    void OnCollisionEnter2D(Collision2D col) {
+        Debug.Log(col.gameObject.tag);
+        if (col.gameObject.tag == "Power_Up") {
+            Destroy(col.gameObject);
+        }
+    }
+}
