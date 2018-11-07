ee674d59cfa08dd8091ca306260eea7e52c65a16
Joshua Bearden <josh.e.bearden@gmail.com>
2018-11-06 21:03:38 -0600

    Revert "Revert "bullshit""
    This reverts commit dd58999de9aa0f802178dd4a6125bc12739c0c52.

--- Surviving College/Assets/scripts/EnterGooseBossScene.cs
+++ Surviving College/Assets/scripts/EnterGooseBossScene.cs
@@ -6,30 +6,21 @@ using UnityEngine.SceneManagement;
 
 public class EnterGooseBossScene : MonoBehaviour
 {
-    public Text pressF;
-    
     // Use this for initialization
     void Start()
     {
     }
     private void OnTriggerStay2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("GooseDoor"))
-        {
-            pressF.text = "Press f to enter";
             if (Input.GetKey(KeyCode.F))
             {
                 SceneManager.LoadScene("GooseBoss");
             }
-        }
-        else
-        {
-            pressF.text = "";
-        }
-    }
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
+
+/*
+public Text pressF;
+pressF.text = "";
+Time.timeScale = 0f;
+*/
